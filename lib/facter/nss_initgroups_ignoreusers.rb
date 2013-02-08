2c9540182aae26fde330fdf128dc9a1dadad26b3
stanlee <johan@lyheden.com>
2013-02-08 23:46:14 +0100

    added fact

---
+++ lib/facter/nss_initgroups_ignoreusers.rb
@@ -0,0 +1,12 @@
+# Fact distributed with puppet-pam module
+Facter.add('nss_initgroups_ignoreusers') do
+  confine :kernel => 'Linux'
+  setcode do
+    ignore_users = `cut -f 1 -d':' /etc/passwd || echo `
+    if ignore_users.length > 0
+      ignore_users.gsub(/\n/,',').chomp
+    else
+      ignore_users
+    end
+  end
+end
