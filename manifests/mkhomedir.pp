869e8971241831fc37a331a72d895c6b66b7e927
jlyheden <johan@lyheden.com>
2013-02-09 21:26:11 +0100

    updated pam::mkhomedir docs

--- manifests/mkhomedir.pp
+++ manifests/mkhomedir.pp
@@ -1,3 +1,45 @@
+# == Class: pam::mkhomedir
+#
+# This module manages the pam_mkhomedir.so PAM module.
+# By enabling this module pam can automatically create
+# a users home folder if it does not already exist on
+# the server, which is useful when connecting external
+# user directory services.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Controls the software installation
+#   Valid values: <tt>present</tt>, <tt>absent</tt>, <tt>purge</tt>
+#
+# [*umask*]
+#   Home folder umask
+#   Valid values: <tt>umask</tt>
+#
+# [*skel*]
+#   Path to skeleton directory used as a template for the home directory
+#   Valid values: <tt>/path/to/skel/dir</tt>
+#
+# === Sample Usage
+#
+# * Installing with default settings
+#   class { 'pam::mkhomedir': }
+#
+# * Uninstalling the software
+#   class { 'pam::mkhomedir': ensure => absent }
+#
+# === Supported platforms
+#
+# This module has been tested on the following platforms
+# * Ubuntu LTS 10.04
+#
+# To add support for other platforms, edit the params.pp file and provide
+# settings for that platform.
+#
+# === Author
+#
+# Johan Lyheden <johan.lyheden@artificial-solutions.com>
+#
 class pam::mkhomedir (  $ensure = 'present',
                         $umask = $pam::params::umask,
                         $skel = $pam::params::skel ) inherits pam::params {
