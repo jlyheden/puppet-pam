8327295ad289cd843439c99d2645b047276dd49b
jlyheden <johan@lyheden.com>
2013-02-09 16:53:28 +0100

    added mkhomedir stuff

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,6 +15,10 @@ class pam::params {
   $source_dir = undef
   $source_dir_purge = undef
 
+  # pam mkhomedir
+  $umask = '0022'
+  $skel = '/etc/skel'
+
   # This mandates which distributions are supported
   # To add support for other distributions simply add
   # a matching regex line to the operatingsystem fact
@@ -24,6 +28,9 @@ class pam::params {
       $package = [ 'libpam0g', 'libpam-modules', 'libpam-runtime' ]
       # ldap 
       $ldap_package = 'libpam-ldapd'
+      $pam_auth_update_mkhomedir_tmpl = 'pam/pam_mkhomedir.ubuntu.erb'
+      $pam_auth_update_dir = '/usr/share/pam-configs'
+      $pam_auth_update_mkhomedir_file = "${pam_auth_update_dir}/mkhomedir"
     }
     default: {
       fail("Unsupported distribution ${::lsbdistcodename}")
