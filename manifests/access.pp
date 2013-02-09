265920b17d3139cb3efbcd05e8612af89133afc6
jlyheden <johan@lyheden.com>
2013-02-09 20:29:53 +0100

    added access management

--- manifests/access.pp
+++ manifests/access.pp
@@ -4,10 +4,26 @@ class pam::access ( $ensure = 'present',
                     $noaudit = $pam::params::noaudit,
                     $fieldsep = $pam::params::fieldsep,
                     $listsep = $pam::params::listsep,
-                    $nodefgroup = $pam::params::nodefgroup ) inherits pam::params {
+                    $nodefgroup = $pam::params::nodefgroup,
+                    $source = $pam::params::access_source,
+                    $template = $pam::params::access_template ) inherits pam::params {
 
   include pam
 
+  # Input validation
+  validate_re($ensure, [ 'present', 'absent' ])
+
+  $manage_file_source = $source ? {
+    ''        => undef,
+    default   => $source,
+  }
+  $manage_file_content = $template ? {
+    ''        => undef,
+    default   => template($template),
+  }
+
+  # Sanitized string builder variables
+  # for pam_access.so options
   $accessfile_entry = $accessfile ? {
     undef   => '',
     ''      => '',
@@ -38,17 +54,62 @@ class pam::access ( $ensure = 'present',
     false   => '',
     default => fail("Unsupported nodefgroup value ${nodefgroup}")
   }
-
   $pam_access_parameters = "${accessfile_entry}${debug_entry}${noaudit_entry}${fieldsep_entry}${listsep_entry}${nodefgroup_entry}"
 
-  file { 'pam_auth_update_access_file':
-    ensure  => $ensure,
-    path    =>$pam::params::pam_auth_update_access_file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    content => template($pam::params::pam_auth_update_access_tmpl),
-    notify  => Exec['pam_auth_update']
+  # Ubuntu uses pam-auth-update to build pam configuration
+  case $::lsbdistcodename {
+    ubuntu: {
+      file { 'pam_auth_update_access_file':
+        ensure  => $ensure,
+        path    =>$pam::params::pam_auth_update_access_file,
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        content => template($pam::params::pam_auth_update_access_tmpl),
+        notify  => Exec['pam_auth_update']
+      }
+    }
+    default: {
+      fail("Unsupported distribution ${::lsbdistcodename}")
+    }
+  }
+
+  case $ensure {
+    present: {
+
+      # use fragments if file source not provided
+      if $manage_file_source == undef and $manage_file_template == undef {
+        concat { $accessfile_entry:
+          ensure  => $ensure,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644'
+        }
+        concat::fragment { '10_pam_access_conf_head':
+          target  => $accessfile_entry,
+          content => "# MANAGED BY PUPPET\n",
+          order   => '10'
+        }
+        concat::fragment { '90_pam_access_conf_foot':
+          target  => $accessfile_entry,
+          content => "-:ALL EXCEPT root: ALL\n",
+          order   => '90'
+        }
+        Concat::Fragment <| tag == 'pam_access' |>
+      # otherwise manage file as usual
+      } else {
+        file { $accessfile_entry:
+          ensure  => present,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => $manage_file_template,
+          source  => $manage_file_source
+        }
+      }
+
+    }
+    default: {} # leave files as is in any other case
   }
 
 }
---
+++ manifests/access/entry.pp
@@ -0,0 +1,33 @@
+define pam::access::entry ( $object,
+                            $ensure = 'present',
+                            $object_type = 'user',
+                            $permission = 'allow',
+                            $origins = 'ALL' ) {
+
+  include pam::params
+
+  # Parameter validation and string building
+  $permission_real = $permission ? {
+    'allow' => '+',
+    'deny'  => '-',
+    default => fail("Unsupported permission ${permission}. Valid values are: allow, deny")
+  }
+  $name_real = $object_type ? {
+    'user'  => "20_pam_access_conf_user_${object}",
+    'group' => "20_pam_access_conf_group_${object}",
+    default => fail("Unsupported object_type ${object_type}. Valid values are: user, group")
+  }
+  $content_real = $object_type ? {
+    'user'  => "${permission} : ${object} : ${origins}\n",
+    'group' => "${permission} : (${object}) : ${origins}\n",
+    default => fail("Unsupported object_type ${object_type}. Valid values are: user, group")
+  }
+
+  @concat::fragment { $name_real:
+    target  => $pam::access::accessfile_entry,
+    content => $content_real,
+    order   => '20',
+    tag     => 'pam_access'
+  }
+
+}
