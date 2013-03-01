dc2f50e5c40dc54291cbf5a1889809fd60916036
jlyheden <johan@lyheden.com>
2013-03-02 00:26:58 +0100

    puppet lint

--- manifests/params.pp
+++ manifests/params.pp
@@ -36,7 +36,7 @@ class pam::params {
     'precise','lucid': {
       # base
       $package = [ 'libpam0g', 'libpam-modules', 'libpam-runtime' ]
-      # ldap 
+      # ldap
       $ldap_package = 'libpam-ldapd'
       $pam_auth_update_mkhomedir_tmpl = 'pam/pam_auth_update/mkhomedir.erb'
       $pam_auth_update_access_tmpl = 'pam/pam_auth_update/access.erb'
