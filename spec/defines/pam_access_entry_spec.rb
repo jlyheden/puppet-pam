f136499998a628e15b84154a88dba4d56bcf7ddd
jlyheden <johan@lyheden.com>
2013-03-02 10:41:38 +0100

    updated spec test for pam::access::entry

--- spec/defines/pam_access_entry_spec.rb
+++ spec/defines/pam_access_entry_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 
 describe 'pam::access::entry' do
 
+  # set depending facts
   let (:facts) { {
-    :lsbdistcodename  => 'lucid',
     :operatingsystem  => 'Ubuntu',
     :concat_basedir   => '/var/lib/puppet/concat'
-  } }
+  } } 
+
   let (:pre_condition) do [
     'class { "pam": }',
     'class { "pam::access": }'
   ]
   end
 
-  context 'add access to Domain Users group' do
+  context 'with object => Domain Users, object_type => group, permission => allow' do
     let (:name) { 'allow_domain_users_group' }
     let (:title) { 'allow_domain_users_group' }
     let (:params) { {
@@ -26,7 +27,7 @@ describe 'pam::access::entry' do
     end
   end
 
-  context 'add access to bogus user' do
+  context 'with object => bogus, object_type => user, permission => allow' do
     let (:name) { 'allow_bogus_user' }
     let (:title) { 'allow_bogus_user' }
     let (:params) { {
@@ -38,5 +39,35 @@ describe 'pam::access::entry' do
       should contain_concat__fragment('20_pam_access_conf_user_bogus').with_content(/\+:bogus:ALL\n/)
     end
   end 
+
+  context 'with object => bogus, object_type => fail, permission => allow' do
+    let (:name) { 'allow_bogus_user' }
+    let (:title) { 'allow_bogus_user' }
+    let (:params) { {
+      :object       => 'bogus',
+      :object_type  => 'fail',
+      :permission   => 'allow'
+    } }
+    it do
+      expect {
+        should include_class('pam::access')
+      }.to raise_error(Puppet::Error, /Unsupported object_type fail. Valid values are: user, group/)
+    end
+  end
+
+  context 'with object => bogus, object_type => user, permission => maybe_allow' do
+    let (:name) { 'allow_bogus_user' }
+    let (:title) { 'allow_bogus_user' }
+    let (:params) { {
+      :object       => 'bogus',
+      :object_type  => 'user',
+      :permission   => 'maybe_allow'
+    } }
+    it do
+      expect {
+        should include_class('pam::access')
+      }.to raise_error(Puppet::Error, /Unsupported permission maybe_allow. Valid values are: allow, deny/)
+    end
+  end
 
 end
